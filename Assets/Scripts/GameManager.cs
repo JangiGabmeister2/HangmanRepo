72dffcf91d51592e336a3ddd820ad54bec35ac5c
JangiGabmeister2 <109714078+JangiGabmeister2@users.noreply.github.com>
2022-11-17 17:50:16 +1100

    added arrays for wordlist, drawings, character holders

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public string[] words;
     public GameObject[] drawings;
+    public Text[] characterHolders;
+
+    private int wordLength;
+
+    private bool drawingComplete = false;
+    private bool wordComplete = false;
 
     public void ChooseRandomWord()
     {
-
+        //https://www.youtube.com/watch?v=EIUHnp-fxAg
     }
 }
